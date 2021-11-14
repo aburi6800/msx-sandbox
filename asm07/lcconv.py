f0f155b9ca069e079009c680efa346af85758a45
= <=>
2021-11-15 00:37:44 +0900

    lcconvの不具合修正、コマンドラインからの実行対応、曲データ追加

--- asm07/lcconv.py
+++ asm07/lcconv.py
@@ -34,263 +34,283 @@
 import json
 import os
 
-class lcconv:
+#lcc = lcconv()
+#lcc.execute(argv)
+#sys.exit(0)
+
+#class lcconv:
+'''
+lcconvクラス
+'''
+# 出力データクラス
+dc = None
+
+#def __init__(self):
+#    '''
+#    クラス初期化
+#    '''
+
+def execute(argv):
     '''
-    lcconvクラス
+    変換処理実行
     '''
-    # 出力データクラス
-    dc = None
+    # 出力データクラスを初期化
+    dc = dataClass(argv[1])
 
-    def __init__(self, jsonFileName:str):
+    # 変換処理実行
+    dc.convert()
+
+    # 出力データクラスからファイルに出力
+    dc.export()
+
+class dataClass:
+    '''
+    出力データクラス
+    '''
+    # jsonデータのリスト
+    data_header = []
+    data_body = []
+
+    # ダンプデータ
+    dumpData = [0, 0, 0]
+
+    # ベースとなるspeed値
+    speed = 0
+
+    # 出力ファイル名
+    outFileName = ""
+
+    # 各値の退避変数を初期化
+    svVoice = None
+    svNote = None
+    svVolume = None
+    svNoiseTone = None
+    svMixing = None
+
+    def __init__(self, jsonFileName:str = ""):
         '''
-        クラス初期化
+        初期化処理
         '''
-        # 出力データクラスを初期化
-        self.dc = self.dataClass(jsonFileName)
+        # 引数のjsonFileNameのjsonファイルを読み込み
+        filePath = os.path.normpath(os.path.join(os.path.dirname(__file__), jsonFileName))
+        with open(filePath) as f:
+            df_header = f.readline()
+            df_body = f.readline()
 
-    def execute(self, outFileName:str):
+        # 出力ファイル名を設定
+        self.outFileName = os.path.splitext(jsonFileName)[0]
+        print("Convert [" + jsonFileName + "] to [" + self.outFileName + ".asm]")
+
+        # jsonデータをパース
+        self.data_header = json.loads(df_header)
+        self.data_body = json.loads(df_body)
+
+    def convert(self):
         '''
-        変換処理実行
+        変換処理
         '''
-        # 変換処理実行
-        self.dc.convert()
+        # speed値取得
+        self.speed = int((self.data_body["speed"]) / 1.8)
+        print("speed = " + str(self.speed))
 
-        # 出力データクラスからファイルに出力
-        self.dc.export(outFileName)
+        # 各チャネルのデータを取得
+        channels = (self.data_body["channels"])["channels"]
+        channelList = list(channels)
 
-    class dataClass:
+        totalSize = 0
+
+        # チャネル1～3に大してダンプデータ作成
+        for i in range(3):
+            self.dumpData[i] = self.makeDumpData(channelList[i])
+            print("track" + str(i+1) + ":" + str(len(self.dumpData[i])) + "bytes.")
+            totalSize += len(self.dumpData[i])
+
+        print("total :" + str(totalSize) + "bytes.")
+
+    def makeDumpData(self, argData):
         '''
-        出力データクラス
+        ダンプデータ作成処理
         '''
-        # jsonデータのリスト
-        data_header = []
-        data_body = []
 
-        # ダンプデータ
-        dumpData = [0, 0, 0]
+        # 空データカウント
+        noneCount = 0
 
-        # ベースとなるspeed値
-        speed = 0
-
-        # 出力ファイル名
-        outfilePath = ""
+        # 終了判定フラグ
+        isTerminate = False
 
         # 各値の退避変数を初期化
-        svVoice = None
-        svNote = None
-        svVolume = None
-        svNoiseTone = None
-        svMixing = None
+        self.svVoice = None
+        self.svNote = None
+        self.svVolume = None
+        self.svNoiseTone = None
+        self.svMixing = None
 
-        def __init__(self, jsonFileName:str = ""):
-            '''
-            初期化処理
-            '''
-            # 引数のjsonFileNameのjsonファイルを読み込み
-            filePath = os.path.normpath(os.path.join(os.path.dirname(__file__), jsonFileName))
-            with open(filePath) as f:
-                df_header = f.readline()
-                df_body = f.readline()
+        # 音長
+        time = 0
 
-            # jsonデータをパース
-            self.data_header = json.loads(df_header)
-            self.data_body = json.loads(df_body)
+        sv = {}
 
-        def convert(self):
-            '''
-            変換処理
-            '''
-            # speed値取得
-            self.speed = int((self.data_body["speed"]) / 1.8)
-            print("speed = " + str(self.speed))
+        # データバッファ
+        buffer = []
 
-            # 各チャネルのデータを取得
-            channels = (self.data_body["channels"])["channels"]
-            channelList = list(channels)
+        # 'sl'要素を取り出す
+        sl = argData["sl"]
 
-            # チャネル1～3に大してダンプデータ作成
-            for i in range(3):
-                self.dumpData[i] = self.makeDumpData(channelList[i])
-                print(self.dumpData[i])
+        # sl要素の全てに対して繰り返す(0～15)
+        for vl in sl:
 
-        def makeDumpData(self, argData):
-            '''
-            ダンプデータ作成処理
-            '''
-
-            # 空データカウント
-            noneCount = 0
-
-            # 終了判定フラグ
-            isTerminate = False
-
-            # 各値の退避変数を初期化
-            self.svVoice = None
-            self.svNote = None
-            self.svVolume = None
-            self.svNoiseTone = None
-            self.svMixing = None
-
-            # 音長
+            sv = vl["vl"][0]
+            #音長をリセット
             time = 0
 
-            sv = {}
+            # vl要素の全てに対して繰り返す(0～31)
+            for v in vl["vl"]:
 
-            # データバッファ
-            buffer = []
+                # voice,tone,volumeのいずれかが直前のデータと違っていたら、退避していaddBufferを呼ぶ
+                # ただし一番最初のデータ時は直前の値が全てNoneなので何もしない
+                if v["n"] != sv["n"] or v["id"] != sv["id"] or v["x"] != sv["x"]:
+                    buffer += self.addBuffer(sv, time)
 
-            # 'sl'要素を取り出す
-            sl = argData["sl"]
+                    # データを退避
+                    sv = v
 
-            # sl要素の全てに対して繰り返す(0～15)
-            for vl in sl:
+                    #音長をリセット
+                    time = 0
 
-                sv = vl["vl"][0]
-                #音長をリセット
-                time = 0
+                # 音長をカウント
+                time += self.speed
 
-                # vl要素の全てに対して繰り返す(0～31)
-                for v in vl["vl"]:
-
-                    # voice,tone,volumeのいずれかが直前のデータと違っていたら、退避していaddBufferを呼ぶ
-                    # ただし一番最初のデータ時は直前の値が全てNoneなので何もしない
-                    if v["n"] != sv["n"] or v["id"] != sv["id"] or v["x"] != sv["x"]:
-                        buffer += self.addBuffer(sv, time)
-
-                        # データを退避
-                        sv = v
-
-                        #音長をリセット
-                        time = 0
-
-                    # 音長をカウント
-                    time += self.speed
-
-                    # トーン=None and ボリューム値=0の場合は、noneCountをインクリメント、以外はリセット
-                    if (v["n"] is None):
-                        noneCount += 1
-                    else:
-                        noneCount = 0
-
-                    # NoneCout=32ならループ終了
-                    if noneCount == 32:
-                        isTerminate = True
-                        break
-
-                if isTerminate == False:
-                    buffer += self.addBuffer(v, time)
+                # トーン=None and ボリューム値=0の場合は、noneCountをインクリメント、以外はリセット
+                if (v["n"] is None):
+                    noneCount += 1
                 else:
+                    noneCount = 0
+
+                # NoneCout=32ならループ終了
+                if noneCount == 32:
+                    isTerminate = True
                     break
 
-            return buffer
-
-        def addBuffer(self, vl, time):
-            '''
-            バッファ追加処理\n
-            引数の値からbufferにデータを追加する。\n
-            ただし、前回の設定値から変わっていないパラメータは設定しない。\n
-            追加するのは以下順とする。\n
-            1) mixing\n
-            2) volme\n
-            3) note or noiseTone\n
-            '''
-            dataList = []
-
-            # voiceが前回の設定値から変わったか判定する
-            # 変わった場合はコマンドとPSGR#6,#7の設定値をバッファに出力する
-            if self.svVoice != vl["id"]:
-                mixing = self.getMixingValue(vl["id"])
-                if mixing != self.svMixing:
-                    # ミキシングの値が変わった場合のみバッファに出力する
-                    dataList += ["201", mixing]
-                    self.svMixing = mixing
-                    self.svVoice = vl["id"]
-
-            # volumeが前回の設定値から変わったか判定する
-            # 変わった場合はコマンドとPSGR#8〜10に設定値をバッファに出力する
-            if self.svVolume != vl["x"]:
-                if vl["n"] == None:
-                    # なぜかnoteがNoneでもvolumeが設定されていることがあるため、対処する
-                    vl["x"] = 0
-                dataList += ["200", str(self.getVolumeValue(vl["x"]))]
-                self.svVolume = vl["x"]
-
-            # noteは無条件でバッファに出力する
-            if self.svMixing == "%10":
-                # トーンの時の処理
-                dataList += [str(self.getNoteValue(vl["n"])), str(time)]
+            if isTerminate == False:
+                buffer += self.addBuffer(v, time)
             else:
-                # ノイズの時の処理
-                noiseTone = self.getNoiseToneValue(vl["n"])
-                if noiseTone != self.svNoiseTone:
-                    dataList += ["202", str(noiseTone)]
-                    self.svNoiseTone = noiseTone
-                dataList += [str(self.getNoteValue(vl["n"])), str(time)]
+                break
 
-            return dataList
+        return buffer
+
+    def addBuffer(self, vl, time):
+        '''
+        バッファ追加処理\n
+        引数の値からbufferにデータを追加する。\n
+        ただし、前回の設定値から変わっていないパラメータは設定しない。\n
+        追加するのは以下順とする。\n
+        1) mixing\n
+        2) volme\n
+        3) note or noiseTone\n
+        '''
+        dataList = []
+
+        # voiceが前回の設定値から変わったか判定する
+        # 変わった場合はコマンドとPSGR#6,#7の設定値をバッファに出力する
+        if self.svVoice != vl["id"] and vl["id"] != None:
+            mixing = self.getMixingValue(vl["id"])
+            if mixing != self.svMixing:
+                # ミキシングの値が変わった場合のみバッファに出力する
+                dataList += ["201", mixing]
+                self.svMixing = mixing
+                self.svVoice = vl["id"]
+
+        # volumeが前回の設定値から変わったか判定する
+        # 変わった場合はコマンドとPSGR#8〜10に設定値をバッファに出力する
+        if vl["n"] == None:
+            # 一度noteを置いて削除した場合、noteがNoneでもvolumeが設定されているため、対処する
+            vl["x"] = 0
+        if self.svVolume != vl["x"]:
+            dataList += ["200", str(self.getVolumeValue(vl["x"]))]
+            self.svVolume = vl["x"]
+
+        # noteは無条件でバッファに出力する
+        if self.svMixing == "%10":
+            # トーンの時の処理
+            dataList += [str(self.getNoteValue(vl["n"])), str(time)]
+        else:
+            # ノイズの時の処理
+            noiseTone = self.getNoiseToneValue(vl["n"])
+            if noiseTone != self.svNoiseTone:
+                dataList += ["202", str(noiseTone)]
+                self.svNoiseTone = noiseTone
+            dataList += [str(self.getNoteValue(vl["n"])), str(time)]
+
+        return dataList
 
 
-        def getNoteValue(self, tone:int) -> int:
-            '''
-            トーン値取得処理
-            '''
-            return (tone - 24 if tone != None else 0)
+    def getNoteValue(self, tone:int) -> int:
+        '''
+        トーン値取得処理
+        '''
+        return (tone - 24 if tone != None else 0)
 
-        def getNoiseToneValue(self, tone:int) -> int:
-            '''
-            ノイズトーン値取得処理
-            '''
-            return int((107-int(tone if tone != None else 1))/2.59)
+    def getNoiseToneValue(self, tone:int) -> int:
+        '''
+        ノイズトーン値取得処理
+        '''
+        return int((107-int(tone if tone != None else 1))/2.59)
 
-        def getMixingValue(self, voice:int) -> str:
-            '''
-            ミキシング値取得処理
-            '''
-            return ("%01" if voice == 3 else "%10")
+    def getMixingValue(self, voice:int) -> str:
+        '''
+        ミキシング値取得処理
+        '''
+        return ("%01" if voice == 3 else "%10")
 
-        def getVolumeValue(self, volume:int) -> int:
-            '''
-            ボリューム値取得処理
-            '''
-            return int(volume*1.25)
+    def getVolumeValue(self, volume:int) -> int:
+        '''
+        ボリューム値取得処理
+        '''
+        return int(volume*1.25)
 
-        def export(self, outFileName:str):
-            '''
-            ファイルエクスポート処理
-            '''
-            # 出力ファイル名
-            outfilePath = os.path.normpath(os.path.join(os.path.dirname(__file__), outFileName))
+    def export(self):
+        '''
+        ファイルエクスポート処理
+        '''
+        # 出力ファイル名
+        outFilePath = os.path.normpath(os.path.join(os.path.dirname(__file__), self.outFileName + ".asm"))
 
-            with open(outfilePath, mode="w") as f:
-                # ヘッダー情報
-                f.write("    DB  0\n")
-                for idx in range(3):
-                    if len(self.dumpData[idx]) > 0:
-                        f.write("    DW  TRK0" + str(idx+1) + "\n")
-                    else:
-                        f.write("    DW  $0000\n")
+        with open(outFilePath, mode="w") as f:
+            # ヘッダー情報
+            f.write("_" + self.outFileName + ":\n")
+            f.write("    DB  0\n")
+            for idx in range(3):
+                if len(self.dumpData[idx]) > 0:
+                    f.write("    DW  _" + self.outFileName + "_TRK" + str(idx+1) + "\n")
+                else:
+                    f.write("    DW  $0000\n")
 
-                # 各チャンネルのデータ
-                for idx, ch in enumerate(self.dumpData):
-                    if len(ch) == 0:
-                        break
-                    else:
-                        f.write("TRK0" + str(idx+1) + ":\n")
-                        s = ""
-                        for i, v in enumerate(ch):
-                            if i % 16 == 0:
-                                if s != "":
-                                    f.write("    DB  " + s + "\n")
-                                s =  v
-                            else:
-                                s += ", " + v
-                        if s != "":
-                            f.write("    DB  " + s + "\n")
-                        f.write("    DB  255\n")
+            # 各チャンネルのデータ
+            for idx, ch in enumerate(self.dumpData):
+                if len(ch) == 0:
+                    break
+                else:
+                    f.write("_" + self.outFileName + "_TRK" + str(idx+1) + ":\n")
+                    s = ""
+                    for i, v in enumerate(ch):
+                        if i % 16 == 0:
+                            if s != "":
+                                f.write("    DB  " + s + "\n")
+                            s =  v
+                        else:
+                            s += ", " + v
+                    if s != "":
+                        f.write("    DB  " + s + "\n")
+                    # @ToDo : ループ指定の有無で254(ループあり)、255(ループなし)を指定可能としたい
+                    f.write("    DB  255\n")
+
+        print("export complete.")
 
 if __name__ == "__main__":
     '''
     アプリケーション実行
     '''
-    c = lcconv("./18.jsonl")
-    c.execute("./out.asm")
+#    execute(["", "19.jsonl"])
+
+    import sys
+    execute(sys.argv)
